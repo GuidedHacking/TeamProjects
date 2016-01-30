643bba23bec9c52badab257370d58440d67ba504
Alexx95 <gh.drphil@gmail.com>
2016-01-30 14:24:09 +0200

    Updated

--- GH_Project1/GH_Project1/Main.cpp
+++ GH_Project1/GH_Project1/Main.cpp
@@ -1,9 +1,15 @@
+/*Made by Alex 30/1/2016
+
+30/1/2016 Update:
+Added a variable containing a string, which is printed as output.*/
 #include <Windows.h>
 #include <iostream>
 
 int main()
 {
-	std::cout << "Hello World! \n";
+	const char* Hello = "Hello World!";
+
+	std::cout << Hello << "\n";
 
 	std::cin.get();
 
