2b77ae2e2c90d378cfcb884465853e89ded6126e
Krampus1025 <gingerbreadman1025@gmail.com>
2016-01-30 18:09:47 -0500

    Added test function

--- GH_Project1/GH_Project1/Main.cpp
+++ GH_Project1/GH_Project1/Main.cpp
@@ -11,6 +11,9 @@ Added useless functions for merge testing.
 
 30/1/2016 Update|Alex
 Added a more polite output.
+
+30/1/2016 Update|Solaire
+Added a test function.
 */
 
 #include <Windows.h>
@@ -18,13 +21,40 @@ Added a more polite output.
 
 #include "Interface.h"
 
+enum class Operation {
+	ADD,
+	SUB,
+	MUL,
+	DIV,
+	MOD
+};
+
+template <class T>
+T mathematical(T x, T y, Operation o);
+
 cInterface Interface;
 
 int main()
 {
 	Interface.printGUI();
+	std::cout << mathematical<int>(12, 4, Operation::ADD) << std::endl;
 
 	std::cin.get();
 
 	return 1;
+}
+
+template <class T>
+T mathematical(T x, T y, Operation o) 
+{
+	switch (o) 
+	{
+	case Operation::ADD: return x + y;
+	case Operation::SUB: return x - y;
+	case Operation::MUL: return x * y;
+	case Operation::DIV: return x / y;
+	case Operation::MOD: return x % y;
+	}
+
+	return 0;
 }
