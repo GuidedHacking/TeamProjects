0d6b870b184e3911d33b0dacf91e55eec012c2ae
Alexx95 <gh.drphil@gmail.com>
2016-01-31 17:39:26 +0200

    fwefew

--- MechWarrior/MechWarrior/Main.cpp
+++ MechWarrior/MechWarrior/Main.cpp
@@ -10,7 +10,7 @@ BOOL WINAPI DllMain(HMODULE hMod, DWORD dwReason, LPVOID lpvReserved)
 {
 	if (dwReason == DLL_PROCESS_ATTACH)
 	{
-		CreateThread(0, 0, MainThread, 0, 0, 0);//test comment
+		CreateThread(0, 0, MainThread, 0, 0, 0);
 	}
 	return TRUE;
 }
